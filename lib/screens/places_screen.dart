5252b3edba3ac7a3c77ae50587d069d4e0e8a049
vibhuti310 <vibhutigupta2002@gmail.com>
2024-01-18 14:09:18 +0530

    Riverpod and new screens

--- lib/screens/places_screen.dart
+++ lib/screens/places_screen.dart
@@ -1,23 +1,31 @@
+import 'package:favourite_places/providers/user_places.dart';
+import 'package:favourite_places/screens/add_places.dart';
 import 'package:favourite_places/widgets/places_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PlacesScreen extends StatelessWidget {
+class PlacesScreen extends ConsumerWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Places'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (ctx) => const AddPlaceScreen()));
+            },
             icon: const Icon(Icons.add),
           ),
         ],
       ),
-      body: const PlacesList(
-        places: [],
+      body: PlacesList(
+        places: userPlaces,
       ),
     );
   }
