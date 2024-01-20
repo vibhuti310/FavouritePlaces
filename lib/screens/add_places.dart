8a747e5195d204f9b5f15766a964b88bcfe9c8c2
vibhuti310 <vibhutigupta2002@gmail.com>
2024-01-20 11:53:38 +0530

    image_input file

--- lib/screens/add_places.dart
+++ lib/screens/add_places.dart
@@ -1,4 +1,5 @@
 import 'package:favourite_places/providers/user_places.dart';
+import 'package:favourite_places/widgets/image_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -49,6 +50,9 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               controller: _titleController,
               style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
             ),
+
+            ImageInput(),
+
             const SizedBox(
               height: 16,
             ),
